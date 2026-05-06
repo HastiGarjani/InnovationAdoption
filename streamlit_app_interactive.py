5155de6ec020a077c38dede68d8e5f651d280b24
HastiGarjani <117920223+HastiGarjani@users.noreply.github.com>
2026-05-06 09:35:56 +0000

    Changed model

--- streamlit_app_interactive.py
+++ streamlit_app_interactive.py
@@ -279,7 +279,7 @@ fig1, ax1 = plt.subplots(figsize=(9, 5))
 
 ax1.plot(model_data.index, model_data["Adopted"], marker="o", color="green", label="Adopted")
 ax1.plot(model_data.index, model_data["Considering"], marker="s", color="orange", label="Considering")
-ax1.plot(model_data.index, model_data["Not adopted"], marker="^", color="darkorange", label="Not adopted")
+ax1.plot(model_data.index, model_data["Not adopted"], marker="^", color="red", label="Not adopted")
 
 ax1.set_xlabel("Time step")
 ax1.set_ylabel("Number of agents")
@@ -300,7 +300,7 @@ st.pyplot(fig1)
 pos = nx.spring_layout(model.network, seed=5)
 
 state_color_map = {
-    0: "darkorange",
+    0: "red",
     1: "orange",
     2: "green"
 }
@@ -386,7 +386,7 @@ if st.button("Show all steps"):
             Line2D([0], [0], marker="o", color="w", label="Considering",
                 markerfacecolor="orange", markeredgecolor="black", markersize=10),
             Line2D([0], [0], marker="o", color="w", label="Not adopted",
-                markerfacecolor="darkorange", markeredgecolor="black", markersize=10),
+                markerfacecolor="red", markeredgecolor="black", markersize=10),
         ]
 
         ax.legend(handles=state_legend, loc="upper right")
