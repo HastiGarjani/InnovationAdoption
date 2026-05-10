95ca37a57661d5d3a565ada20914f59083a30fee
HastiGarjani <117920223+HastiGarjani@users.noreply.github.com>
2026-05-10 19:32:29 +0200

    Delete app101.py

--- app101.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-skepticism_ratio = st.slider(
-    "Select skepticism ratio",
-    min_value=0.1,
-    max_value=0.7,
-    value=0.2,
-    step=0.1
-)
-st.write("Your chosen value is", skepticism_ratio)
