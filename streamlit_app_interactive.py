e0d5817ba36c638deaecb7be489d3f9a82bdad79
HastiGarjani <117920223+HastiGarjani@users.noreply.github.com>
2026-05-21 15:13:22 +0000

    design

--- streamlit_app_interactive.py
+++ streamlit_app_interactive.py
@@ -320,6 +320,7 @@ agent_data = model.datacollector.get_agent_vars_dataframe()
 
 final_step = model_data.index.max()
 final_agent_data = agent_data.loc[final_step]
+st.markdown("#")
 st.subheader("**Different steps the ABM goes through**")
 # st.button("Show all steps")
 
